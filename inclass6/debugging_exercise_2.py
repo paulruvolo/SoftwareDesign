23e9e3bbda5307b113d765c6b9cff57cc22ca607
paulruvolo <paullundyruvolo@gmail.com>
2014-02-10 01:56:58 -0500

    minor changes

--- inclass6/debugging_exercise_2.py
+++ inclass6/debugging_exercise_2.py
@@ -7,10 +7,11 @@ Created on Sun Feb  9 17:52:54 2014
 
 def factorial(n):
     """ Computes the factorial of the non-negative input integer n """
-    retval = 1
+    return_val = 1
     for i in range(n):
-        retval *= i
-    return retval
+        return_val *= i
+        assert(return_val >= 1)
+    return return_val
 
 if __name__ == '__main__':
     print factorial(5)
--- inclass6/debugging_exercise_3.py
+++ inclass6/debugging_exercise_3.py
@@ -7,16 +7,16 @@ Created on Sun Feb  9 17:52:54 2014
 
 def get_primes(n):
     """ Returns a list of all prime integers in the range [1,n] """
-    retval = []
+    return_val = []
     isPrime = True
 
     for i in range(1,n+1):
-        for j in range(2,i):
+        for j in range(1,i):
             if i % j == 0:
                 isPrime = False
         if isPrime:
-            retval.append(i)
-    return retval
+            return_val.append(i)
+    return return_val
 
 
 if __name__ == '__main__':
