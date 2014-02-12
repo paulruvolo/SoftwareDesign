b2cfa5bbf05d4ffaa215811a58edcc359788f73d
paulruvolo <paullundyruvolo@gmail.com>
2014-02-11 23:20:55 -0500

    stubbed out materials for hw4

---
+++ hw4/random_art.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 11 11:34:57 2014
+
+@author: pruvolo
+"""
+
+# you do not have to use these particular modules, but they may help
+from random import randint
+import Image
+
+def build_random_function(min_depth, max_depth):
+    # your doc string goes here
+
+    # your code goes here
+
+def evaluate_random_function(f, x, y):
+    # your doc string goes here
+
+    # your code goes here
+
+def remap_interval(val, input_interval_start, input_interval_end, output_interval_start, output_interval_end):
+    """ Maps the input value that is in the interval [input_interval_start, input_interval_end]
+        to the output interval [output_interval_start, output_interval_end].  The mapping
+        is an affine one (i.e. output = input*c + b).
+    
+        TODO: please fill out the rest of this docstring
+    """
+    # your code goes here
+    
